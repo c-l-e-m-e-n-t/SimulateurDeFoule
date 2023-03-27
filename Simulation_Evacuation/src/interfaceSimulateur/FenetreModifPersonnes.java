f88fc51c9ecd12c487f20b807d34b38fe149279b
Felix de Brandois <felix.debrandois@gmail.com>
2023-03-27 05:40:21 +0200

    Propre

--- Simulation_Evacuation/src/interfaceSimulateur/FenetreModifPersonnes.java
+++ Simulation_Evacuation/src/interfaceSimulateur/FenetreModifPersonnes.java
@@ -1,10 +1,10 @@
-// creation de l'interface graphique de la fenêtre de modification des personnes
 package interfaceSimulateur;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+/** Interface Graphique de la fenètre de modification de personnes. */
 class FenetreModifPersonnes {
     boolean construction  = true;
     public static void main(String[] args) {
@@ -13,64 +13,64 @@ class FenetreModifPersonnes {
         fenetre.setSize(1080, 720);
         fenetre.setLocationRelativeTo(null);
 
-        //creer le container de la fenetre
+        // Créer le container de la fenetre
         Container contenu = fenetre.getContentPane();
         contenu.setLayout(new BorderLayout());
 
-        // creer un bandeau en haut de la fenetre pour afficher le titre
+        // Créer un bandeau en haut de la fenetre pour afficher le titre
         JPanel bandeau = new JPanel();
         bandeau.setBackground(Color.GRAY);
         fenetre.add(bandeau, BorderLayout.NORTH);
-        //ecrire "Mode classique" dans le bandeau
+        // Ecrire "Mode classique" dans le bandeau
         JLabel titre = new JLabel("Modification des personnes");
         bandeau.add(titre);
-        //ajouter un bouton a droite du bandeau home
+        // Ajouter un bouton a droite du bandeau home
         JButton boutonHome = new JButton("Retour a la simulation");
         bandeau.add(boutonHome, BorderLayout.EAST);
 
-        //creer une grille de 5 colonnes cans la partie centrale
+        // Creer une grille de 5 colonnes cans la partie centrale
         JPanel grille5cols = new JPanel();
         grille5cols.setLayout(new GridLayout(3, 5));
         fenetre.add(grille5cols, BorderLayout.CENTER);
 
-        //ajouter une etiquette "vitesse" 
+        // Ajouter une étiquette "vitesse" 
         JLabel etiquetteVitesse = new JLabel("Vitesse");
         grille5cols.add(etiquetteVitesse);
 
-        // ajouter une etiquette "min"
+        // Ajouter une étiquette "min"
         JLabel etiquetteMin = new JLabel("Min");
         grille5cols.add(etiquetteMin);
 
-        // ajouter une case remplissable "min" par des chiffres
+        // Ajouter une case remplissable "min" par des chiffres
         JTextField caseMinVitesse = new JTextField();
         grille5cols.add(caseMinVitesse);
         
-        // ajouter une etiquette "max"
+        // Ajouter une étiquette "max"
         JLabel etiquetteMax = new JLabel("Max");
         grille5cols.add(etiquetteMax);
 
-        //ajouter une case remplissable "max" par des chiffres
+        // Ajouter une case remplissable "max" par des chiffres
         JTextField caseMaxVitesse = new JTextField();
         //reduite la  hauteur de la case
         caseMaxVitesse.setPreferredSize(new Dimension(50, 20));
         grille5cols.add(caseMaxVitesse);
         
-        //ajouter une etiquette "masse" 
+        // Ajouter une étiquette "masse" 
         JLabel etiquetteMasse = new JLabel("Masse");
         grille5cols.add(etiquetteMasse);
 
-        //ajouter une etiquette "reaction" 
+        // Ajouter une etiquette "reaction" 
         JLabel etiquetteReaction = new JLabel("Reaction");
         grille5cols.add(etiquetteReaction);
         
-        //remplir la grille avec des chiffres 1,2,3,4,5,...
+        // Remplir la grille avec des chiffres 1,2,3,4,5,...
         for (int i = 0; i < 10; i++) {
             JLabel etiquette = new JLabel(Integer.toString(i + 1));
             grille5cols.add(etiquette);
         }
         
 
-        //afficher la fenetre
+        // Afficher la fenetre
         fenetre.setVisible(true);
     }
 }
--- Simulation_Evacuation/src/interfaceSimulateur/FenetreSimulation.java
+++ Simulation_Evacuation/src/interfaceSimulateur/FenetreSimulation.java
@@ -12,53 +12,53 @@ class FenetreSimulation {
         fenetre.setSize(1080, 720);
         fenetre.setLocationRelativeTo(null);
 
-        //creer le container de la fenetre
+        // Créer le container de la fenetre
         Container contenu = fenetre.getContentPane();
         contenu.setLayout(new BorderLayout());
 
-        // creer un bandeau en haut de la fenetre pour afficher le titre
+        // Créer un bandeau en haut de la fenetre pour afficher le titre
         JPanel bandeau = new JPanel();
         bandeau.setBackground(Color.GRAY);
         fenetre.add(bandeau, BorderLayout.NORTH);
-        //ecrire "Mode classique" dans le bandeau
+        // Ecrire "Mode classique" dans le bandeau
         JLabel titre = new JLabel("Mode classique");
         bandeau.add(titre);
-        //ajouter un bouton a droite du bandeau home
+        // Ajouter un bouton a droite du bandeau home
         JButton boutonHome = new JButton("Home");
         bandeau.add(boutonHome, BorderLayout.EAST);
 
-        //creer une grilleParamDroite dns la partie droite de la fenetre
+        // Créer une grilleParamDroite dans la partie droite de la fenetre
         JPanel grilleParamDroite = new JPanel();
         grilleParamDroite.setLayout(new GridLayout(11, 1));
         fenetre.add(grilleParamDroite, BorderLayout.EAST);
 
-        //ajouter un bouton lancer la simulation en haut de la grilleParamDroite
+        // Ajouter un bouton lancer la simulation en haut de la grilleParamDroite
         JButton boutonLancerSimu = new JButton("Lancer la simulation");
         grilleParamDroite.add(boutonLancerSimu);
 
 
-        //ajouter une liste déroulante a droite pour selectionner le nombre de personnes au dessus du bouton lancer la simulation dans la grilleParamDroite
-        //creer une liste déroulante de 1 a 500 personnes
+        // Ajouter une liste déroulante a droite pour selectionner le nombre de personnes au dessus du bouton lancer la simulation dans la grilleParamDroite
+        // Creer une liste déroulante de 1 a 500 personnes
         String[] nbPersonnes = new String[500];
         for (int i = 0; i < 500; i++) {
             nbPersonnes[i] = Integer.toString(i + 1);
         }
         JComboBox listeDeroulante = new JComboBox(nbPersonnes);
-        //creer une grille de 2 colonnes
+        // Créer une grille de 2 colonnes
         JPanel grille2cols = new JPanel();
         grille2cols.setLayout(new GridLayout(1, 2));
         grilleParamDroite.add(grille2cols);
-        //ajouter une etiquette "Nombre de personnes" a gauche de la liste déroulante
+        // Ajouter une étiquette "Nombre de personnes" a gauche de la liste déroulante
         JLabel etiquette = new JLabel("Nombre de personnes : ");
         grille2cols.add(etiquette);
-        //ajouter la liste déroulante a droite de l'etiquette
+        // Ajouter la liste déroulante a droite de l'étiquette
         grille2cols.add(listeDeroulante);
 
-        //Sauter une ligne
+        // Sauter une ligne
         JLabel vide = new JLabel(" ");
         grilleParamDroite.add(vide);
 
-        //ajoutre le label param avancés en dessous de la liste déroulante centré
+        // Ajoutre le label param avancés en dessous de la liste déroulante centré
         JLabel paramAvances = new JLabel("--Paramètres avancés--");
         paramAvances.setHorizontalAlignment(JLabel.CENTER);
         grilleParamDroite.add(paramAvances);
@@ -66,15 +66,15 @@ class FenetreSimulation {
         JPanel grilleParamAvancés = new JPanel();
         grilleParamAvancés.setLayout(new GridLayout(3, 2));
         grilleParamDroite.add(grilleParamAvancés);
-        //ajouter le nombre pourcentage d'enfants sous la forme d'une liste déroulante
+        // Ajouter le nombre pourcentage d'enfants sous la forme d'une liste déroulante
         String[] nbEnfants = new String[100];
         for (int i = 0; i < 100; i++) {
             nbEnfants[i] = Integer.toString(i + 1);
         }
         JComboBox listeDeroulanteEnfants = new JComboBox(nbEnfants);
-        //creer une grille de 2 colonnes
+        // Creer une grille de 2 colonnes
         grilleParamDroite.add(grilleParamAvancés);
-        //ajouter une etiquette "Nombre d'enfants" a gauche de la liste déroulante
+        // Ajouter une etiquette "Nombre d'enfants" a gauche de la liste déroulante
         JLabel etiquetteEnfants = new JLabel("Nombre d'enfants (%) : ");
         grilleParamAvancés.add(etiquetteEnfants);
         grilleParamAvancés.add(listeDeroulanteEnfants);
@@ -82,11 +82,11 @@ class FenetreSimulation {
         JLabel etiquettePanique = new JLabel("Mode Panique : ");
         grilleParamAvancés.add(etiquettePanique);
 
-        //ajouter un bouton on/off pour le mode panique vert quand cliqué rouge sinon
+        // Ajouter un bouton on/off pour le mode panique vert quand cliqué rouge sinon
         JToggleButton boutonOnOffPanique = new JToggleButton("On/Off");
         grilleParamAvancés.add(boutonOnOffPanique);
 
-        //ajout du rapport (graphique)
+        // Ajout du rapport (graphique)
 
         JLabel etiquetteRapport = new JLabel("Rapport : ");
         grilleParamAvancés.add(etiquetteRapport);
@@ -94,31 +94,31 @@ class FenetreSimulation {
         JToggleButton boutonOnOffRapport = new JToggleButton("On/Off");
         grilleParamAvancés.add(boutonOnOffRapport);
 
-        //sauter une case
+        // Sauter une case
         grilleParamDroite.add(vide);
 
-        //ajouter un bouton pour configurer les personnes
+        // Ajouter un bouton pour configurer les personnes
         JButton boutonConfigPersonnes = new JButton("Configurer les personnes");
         grilleParamDroite.add(boutonConfigPersonnes);
         
-        //ajouter une section pour afficher les informations sur la simulation
+        // Ajouter une section pour afficher les informations sur la simulation
         JPanel grilleInfo = new JPanel();
         grilleInfo.setLayout(new GridLayout(2, 2));
         grilleParamDroite.add(grilleInfo);
 
-        //ajout d'une section titre avec une case remplissable par l'utilisateur
+        // Ajout d'une section titre avec une case remplissable par l'utilisateur
         JLabel titreInfo = new JLabel("Titre : ");
         grilleInfo.add(titreInfo);
         JTextField titreInfoRemplissable = new JTextField();
         grilleInfo.add(titreInfoRemplissable);
 
-        //ajout d'une section titre avec une case remplissable par l'utilisateur
+        // Ajout d'une section titre avec une case remplissable par l'utilisateur
         JLabel auteurInfo = new JLabel("Auteur : ");
         grilleInfo.add(auteurInfo);
         JTextField auteurInfoRemplissable = new JTextField();
         grilleInfo.add(auteurInfoRemplissable);
 
-        //ajout d'une section commentaires avec une case remplissable par l'utilisateur sur plusieurs lignes
+        // Ajout d'une section commentaires avec une case remplissable par l'utilisateur sur plusieurs lignes
         JLabel commentaireInfo = new JLabel("Commentaires : ");
         grilleParamDroite.add(commentaireInfo);
         JTextArea commentaireInfoRemplissable = new JTextArea();
@@ -130,7 +130,7 @@ class FenetreSimulation {
         scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         grilleParamDroite.add(scroll);
 
-        //ajouter un bandeau en bas de la page pour afficher les auteurs du logiciel
+        // Ajouter un bandeau en bas de la page pour afficher les auteurs du logiciel
         JPanel bandeauBas = new JPanel();
         bandeauBas.setBackground(Color.GRAY);
         fenetre.add(bandeauBas, BorderLayout.SOUTH);
@@ -138,18 +138,15 @@ class FenetreSimulation {
         bandeauBas.add(auteur);
 
 
-        //creation de la zone de simulation
+        // Creation de la zone de simulation
         JPanel cadreSimulation = new JPanel();
         cadreSimulation.setBackground(Color.WHITE);
-        //mettre des bordures autour de la zone de simulation
+        // Mettre des bordures autour de la zone de simulation
         cadreSimulation.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         fenetre.add(cadreSimulation, BorderLayout.CENTER);
 
-        //rendre la fentre visible
+        // Rendre la fentre visible
         fenetre.setVisible(true);
     }
 
 }
-
-
-
