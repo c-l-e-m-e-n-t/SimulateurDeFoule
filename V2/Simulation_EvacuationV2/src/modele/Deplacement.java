ce790576b4d96f65d55417b2c4f0e4334295edf6
ClémentC <59148218+c-l-e-m-e-n-t@users.noreply.github.com>
2023-05-17 22:33:22 +0200

    sans les print

--- V2/Simulation_EvacuationV2/src/modele/Deplacement.java
+++ V2/Simulation_EvacuationV2/src/modele/Deplacement.java
@@ -144,11 +144,11 @@ public class Deplacement {
 			Vecteur a3 = Vecteur.multiplication(forceMurs[i], 1 / m);
 
 			acceleration[i] = Vecteur.somme(Vecteur.somme(a1, a2), a3);
-			
+			/*
 			System.out.println(a1);
 			System.out.println(a2);
 			System.out.println(a3);
-			System.out.println();
+			System.out.println();*/
 		}
 		return acceleration;
 	}
