eca33d8b08b924b449858913b51b0b5ed709798a
OussamaELGUERRAOUI <124736443+OussamaELGUERRAOUI@users.noreply.github.com>
2023-05-28 23:01:31 +0200

    **

--- V2/Simulation_EvacuationV2/src/outils/BureauObstacle.java
+++ V2/Simulation_EvacuationV2/src/outils/BureauObstacle.java
@@ -4,25 +4,47 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
+
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
+
+import javax.swing.JPanel;
+
+
+import affichage.SimulationData;
 
 public class BureauObstacle {
     private int largeur;
     private int longueur;
-    private Color color = Color.BLACK;
-    int x, y;
+    private Color color ;
+    public double x;
+    public double y;
+    boolean actif;
 
-    public BureauObstacle(int x, int y, int largeur, int longueur) {
+    
+   
+ 
+    public BureauObstacle(double x, double y) {
         this.x = x;
         this.y = y;
-        this.largeur = largeur;
-        this.longueur = longueur;
+        this.largeur = 40;
+        this.longueur = 40;
+        this.actif = true;
+        
+    }
+    public BureauObstacle(int largeur, int longueur) {
+        this.x = 100;
+        this.y = 100;
+        this.largeur = 40;
+        this.longueur = 40;
+        this.color = Color.black;
+        this.actif = true;
+       
     }
 
     public Shape getHitbox() {
-        int centerX = x;
-        int centerY = y;
+        double centerX = x;
+        double centerY = y;
         int width = largeur / 2;
         int height = longueur / 2;
         Rectangle2D.Double rect = new Rectangle2D.Double(centerX - width, centerY - height, largeur, longueur);
@@ -39,4 +61,33 @@ public class BureauObstacle {
         g2d.fill(hitbox);
     }
 
-}
+    public void ajouterObstacle(JPanel panel) {
+        panel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent e) {
+                if (actif){
+                    SimulationData.addbureau((double) e.getX(), (double) e.getY());
+                    panel.repaint();
+                }
+            }
+        });
+        
+    }
+
+    public void setActif(boolean actif) {
+        this.actif = actif;
+    }
+
+    boolean getActif() {
+        return this.actif;
+    }
+
+    
+    }
+
+
+
+    
+
+   
+
+
